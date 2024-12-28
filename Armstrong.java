ab39a9a5334495ab01a6fed35508480c68f6087f
Codewithveer49 <veerdhawalkatakdaunde5032@gmail.com>
2024-12-28 19:05:29 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,38 @@
+package practice;
+
+import java.util.Scanner;
+
+public class Armstrong_10000 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        for(int i = 1;i <=100000;i++){
+            if(Armstrong_number(i)){
+                System.out.println(i);
+            }
+        }
+
+
+    }
+    static boolean Armstrong_number(int n){
+        int m = n;
+       int k = 0;
+       int ans = 0;
+       while(n>0){
+        int rem = n%10;
+        k = rem*rem*rem;
+        ans += k;
+        k = 0;
+        n/=10;
+
+       }
+       if (m == ans) {
+        return  true;
+       }
+       else{
+        return false;
+       }
+
+    }
+}
